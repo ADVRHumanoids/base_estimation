affb0c3ba6c7ff8e1869228b5bf46cf3facd3d6e
EnricoMingo <enricomingo@gmail.com>
2021-02-03 17:31:41 +0100

    Added missing files and visualization of weights

---
+++ include/base_estimation/contact_viz.h
@@ -0,0 +1,27 @@
+#ifndef _CONTACT_VIZ_H_
+#define _CONTACT_VIZ_H_
+
+#include <visualization_msgs/MarkerArray.h>
+#include <xbot2/ros/ros_support.h>
+#include <Eigen/Dense>
+
+
+namespace ikbe
+{
+class contact_viz
+{
+public:
+    contact_viz(const std::string& topic_name, XBot::RosSupport* ros);
+
+    bool publish(const std::vector<std::string>& frames, const Eigen::VectorXd& normal_forces);
+
+private:
+    visualization_msgs::MarkerArray _marker_array_msg;
+
+    XBot::PublisherPtr<visualization_msgs::MarkerArray> _pub;
+
+    XBot::Journal _j;
+};
+}
+
+#endif
