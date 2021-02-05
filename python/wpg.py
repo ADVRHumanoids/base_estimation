5e8402d97923ee63a4925f11705cc5ed7a900254
francesco.ruscelli <francesco.ruscelli@iit.it>
2021-02-05 10:03:40 +0100

    set as input variable 'dt' for integrator RK4

--- python/wpg.py
+++ python/wpg.py
@@ -74,7 +74,8 @@ class Stepper:
         # Formulate discrete time dynamics
         # Fixed step Runge-Kutta 4 integrator
         M = 1  # RK4 steps per interval
-        self.F = self.RK4(M, L)
+        self.dt = self.T / self.N / M
+        self.F = self.RK4(M, L, self.dt)
 
         # augment state
         self.z = vertcat(self.p, self.v, self.a, self.l, self.r, self.alpha_l, self.alpha_r)  # state
@@ -91,18 +92,17 @@ class Stepper:
 
         self.XInt = []
 
-    def RK4(self, M, L):
+    def RK4(self, M, L, dt):
         """RK4 Runge-Kutta 4 integrator
         Input:
-            L: objective fuction to integrate
+            L: objective function to integrate
             M: RK steps
             T: final time
-            N: numbr of shooting nodes:
-            x: state varibales
+            N: number of shooting nodes:
+            x: state variables
             u: controls
         """
 
-        DT = self.T / self.N / M
         f = Function('f', [self.x, self.u], [self.xdot, L])
         X0 = self.sym_c.sym('X0', self.x.size()[0])
         U = self.sym_c.sym('U', self.u.size()[0])
@@ -111,11 +111,11 @@ class Stepper:
 
         for j in range(M):
             k1, k1_q = f(X, U)
-            k2, k2_q = f(X + DT / 2 * k1, U)
-            k3, k3_q = f(X + DT / 2 * k2, U)
-            k4, k4_q = f(X + DT * k3, U)
-            X = X + DT / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
-            Q = Q + DT / 6 * (k1_q + 2 * k2_q + 2 * k3_q + k4_q)
+            k2, k2_q = f(X + dt / 2 * k1, U)
+            k3, k3_q = f(X + dt / 2 * k2, U)
+            k4, k4_q = f(X + dt * k3, U)
+            X = X + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+            Q = Q + dt / 6 * (k1_q + 2 * k2_q + 2 * k3_q + k4_q)
 
         return Function('F', [X0, U], [X, Q], ['x0', 'p'], ['xf', 'qf'])
 
