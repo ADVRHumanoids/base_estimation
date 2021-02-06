abb58b97e3c5ace12e52e8862ae2d1be5a9e79f0
francesco.ruscelli <francesco.ruscelli@iit.it>
2021-02-06 18:21:40 +0100

    added more

--- python/Constraint.py
+++ python/Constraint.py
@@ -23,6 +23,10 @@ if __name__ == '__main__':
     ct = Constraint()
     x = list()
     x = cs.SX.sym('X_1', 6)
+    u = cs.SX.sym('U_1', 2)
+
+    lbw = [0, 0, 0, 0, 0, 0] + [0, 0]
+    ubw = [0, 0, 0, 0, 0, 0] + [0, 0]
 
     g = x[0:2] - x[4:6]
 
@@ -35,21 +39,24 @@ if __name__ == '__main__':
     w0 = list()
 
     J = 1
-    w = x
-    g = ct.g
+    g.append(ct.g)
 
-    lbw = ct.lbw
-    ubw = ct.ubw
+    lbw = lbw
+    ubw = ubw
     lbg = ct.lbg
     ubg = ct.ubg
 
-    prob = prob = {'f': J, 'x': cs.vertcat(*w), 'g': cs.vertcat(*g)}
+    w = [None] * (len(x) + len(u))
+    w[0::2] = x
+    w[1::2] = u
+
+    prob = {'f': J, 'x': cs.vertcat(*w), 'g': cs.vertcat(*g)}
     solver = cs.nlpsol('solver', 'ipopt', prob,
                     {'ipopt': {'linear_solver': 'ma27', 'tol': 1e-4, 'print_level': 0, 'sb': 'yes'},
                      'print_time': 0})  # 'acceptable_tol': 1e-4(ma57) 'constr_viol_tol':1e-3
 
     # Solve the NLP
     w0 += list(np.zeros(x.size()[0]))
-    sol = solver(x0=w0, lbx=lbw, ubx=ubw, lbg=lbg,ubg=ubg)
+    sol = solver(x0=w0, lbx=lbw, ubx=ubw, lbg=lbg, ubg=ubg)
 
     w_opt = sol['x'].full().flatten()
