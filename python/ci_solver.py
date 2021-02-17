4907385a8e6da5d25d80de48970b0219880ef2e8
francesco.ruscelli <francesco.ruscelli@iit.it>
2021-02-17 18:04:12 +0100

    added ci_solver class and step_top for step execution on the robot

---
+++ python/ci_solver.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+import xbot_interface.config_options as xbot_opt
+import rospy
+from cartesian_interface.pyci_all import *
+from xbot_interface import xbot_interface as xbot
+from xbot_interface import config_options as co
+import numpy as np
+from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
+import yaml
+
+class CartesianInterfaceSolver:
+    '''
+    inverse kinematics solver by CartesI/O
+    input:
+        model --> model of the robot
+        ik_dt --> dt of the solver
+        ctrl_points --> joint of the robot to control
+
+    '''
+    def __init__(self, model, robot, ik_dt, ctrl_points):
+
+        self.model = model
+        self.robot = robot
+        self.ik_dt = ik_dt
+        self.ctrl_points = ctrl_points
+
+        # to visualize the robot in rviz
+        self.rspub = pyci.RobotStatePublisher(self.model)
+
+        self.ik_pb = self.__make_problem_desc_ik()
+        self.log_path = '/tmp'
+        self.ci = pyci.CartesianInterface.MakeInstance('OpenSot',
+                                                       self.ik_pb,
+                                                       self.model,
+                                                       self.ik_dt,
+                                                       log_path=self.log_path)
+
+        self.ctrl_tasks = []
+        for k in self.ctrl_points.values():
+            self.ctrl_tasks.append(self.ci.getTask(k))
+
+        self.com = self.ci.getTask('com')
+        self.postural = self.ci.getTask('postural')
+
+    def __make_problem_desc_ik(self):
+
+        # cartesI/O stack:
+        # math of task (mot)
+        #  --> ctrl_points
+        #  --> com
+        #  --> postural
+
+        ik_cfg = dict()
+
+        ik_cfg['solver_options'] = {'regularization': 1e-4, 'back-end': 'osqp'}
+
+        ik_cfg['stack'] = [
+            self.ctrl_points.values(), ['com'], ['postural']
+        ]
+
+        ik_cfg['postural'] = {
+            'name': 'postural',
+            'type': 'Postural',
+            'lambda': 0.01,
+        }
+
+        ik_cfg['com'] = {
+            'name': 'com',
+            'type': 'Com',
+            'lambda': 1,
+        }
+
+        for c in self.ctrl_points.values():
+            ik_cfg[c] = {
+                'type': 'Cartesian',
+                'distal_link': c,
+                'lambda': 1
+            }
+
+        ik_str = yaml.dump(ik_cfg)
+
+        return ik_str
+
+    def __ci_solve_integrate(self, t):
+
+        # integrate model
+        if not self.ci.update(t, self.ik_dt):
+            return False
+
+        q = self.model.getJointPosition()
+        qdot = self.model.getJointVelocity()
+        qddot = self.model.getJointAcceleration()
+
+        q += qdot * self.ik_dt + 0.5 * qddot * self.ik_dt ** 2
+        qdot += qddot * self.ik_dt
+
+        self.model.setJointPosition(q)
+        self.model.setJointVelocity(qdot)
+        self.model.update()
+
+        self.robot.setPositionReference(q[6:])
+        self.robot.move()
+
+        return True
+
+    def reach(self, task, goal, duration):
+
+        ## Cartesian part
+        q = np.empty(shape=[self.model.getJointNum(), 0])
+
+        task.setActivationState(pyci.ActivationState.Enabled)
+
+        time_from_reaching = 0.
+        unable_to_solve = 0
+        ci_time = 0.0
+        initialize_trj = False
+
+        CONVERGENCE_TIME = 5.
+        UNABLE_TO_SOLVE_MAX = 5
+
+        task.setPoseTarget(goal, duration)
+
+        while task.getTaskState() == pyci.State.Reaching or time_from_reaching <= CONVERGENCE_TIME:
+
+            q = np.hstack((q, self.model.getJointPosition().reshape(self.model.getJointNum(), 1)))
+
+            if not self.__ci_solve_integrate(ci_time):
+                print('Unable to solve!!!')
+                unable_to_solve += 1
+                print(unable_to_solve)
+                # break
+                if unable_to_solve >= UNABLE_TO_SOLVE_MAX:
+                    print("Maximum number of unable_to_solve reached: ")
+                    print(unable_to_solve)
+                    return q, False
+            else:
+                unable_to_solve = 0
+
+            ci_time += self.ik_dt
+
+            if task.getTaskState() == pyci.State.Online:
+                if not initialize_trj:
+                    initialize_trj = True
+
+                time_from_reaching += self.ik_dt
+
+            self.rspub.publishTransforms('ci')
+
+    def __getitem__(self, idx):
+        return self.ctrl_tasks[idx]
+
+    def getTasks(self):
+        return self.ctrl_tasks, self.com
+
+    def sendTrajectory(self, task, traj):
+
+        q = np.empty(shape=[self.model.getJointNum(), 0])
+
+        task.setActivationState(pyci.ActivationState.Enabled)
+
+        unable_to_solve = 0
+        ci_time = 0.0
+
+        UNABLE_TO_SOLVE_MAX = 5
+
+        task.setPoseReference(traj)
+
+        if not self.__ci_solve_integrate(ci_time):
+            print('Unable to solve!!!')
+            unable_to_solve += 1
+            print(unable_to_solve)
+            # break
+            if unable_to_solve >= UNABLE_TO_SOLVE_MAX:
+                print("Maximum number of unable_to_solve reached: ")
+                print(unable_to_solve)
+                return q, False
+        else:
+            unable_to_solve = 0
+
+        ci_time += self.ik_dt
+
+        self.rspub.publishTransforms('ci')
+
+
+if __name__ == '__main__':
+
+    np.set_printoptions(precision=3, suppress=True)
+
+    rospy.init_node('ci_solver_try')
+    roscpp_init('ci_solver_try', [])
+
+    opt = xbot_opt.ConfigOptions()
+
+    urdf = rospy.get_param('/xbotcore/robot_description')
+    srdf = rospy.get_param('/xbotcore/robot_description_semantic')
+
+    opt = co.ConfigOptions()
+    opt.set_urdf(urdf)
+    opt.set_srdf(srdf)
+    opt.generate_jidmap()
+    opt.set_bool_parameter('is_model_floating_base', True)
+    opt.set_string_parameter('model_type', 'RBDL')
+    opt.set_string_parameter('framework', 'ROS')
+    model = xbot.ModelInterface(opt)
+    robot = xbot.RobotInterface(opt)
+
+    robot.sense()
+    model.syncFrom(robot)
+    model.update()
+
+    ctrl_points = {0: 'l_sole', 1: 'r_sole'}
+    ci_solver = CartesianInterfaceSolver(model=model, robot=robot, ik_dt=0.01, ctrl_points=ctrl_points)
+    print 'Created cartesian interface.'
+
+    one_task = ci_solver[0]
+
+    starting_pose = Affine3()
+    starting_pose.translation = model.getPose(one_task.getName()).translation
+    starting_pose.linear = model.getPose(one_task.getName()).linear
+
+    traj = Affine3()
+    traj.translation = starting_pose.translation
+    traj.linear = starting_pose.linear
+
+    for i in range(100):
+
+        traj.translation[2] = traj.translation[2] + 0.001
+
+        print(traj.translation)
+        ci_solver.sendTrajectory(ci_solver[0], traj)
+
+        rospy.sleep(0.01)
+
+
---
+++ python/step_top.py
@@ -0,0 +1,170 @@
+from ci_solver import CartesianInterfaceSolver
+from wpg import Stepper
+import numpy as np
+import casadi as cs
+
+import xbot_interface.config_options as xbot_opt
+import rospy
+from cartesian_interface.pyci_all import *
+from xbot_interface import xbot_interface as xbot
+from xbot_interface import config_options as co
+import numpy as np
+from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
+import yaml
+
+import matplotlib.pyplot as plt
+
+np.set_printoptions(precision=3, suppress=True)
+
+rospy.init_node('step_top')
+roscpp_init('step_top', [])
+
+# PREPARE ROBOT
+opt = xbot_opt.ConfigOptions()
+
+urdf = rospy.get_param('/xbotcore/robot_description')
+srdf = rospy.get_param('/xbotcore/robot_description_semantic')
+
+opt = co.ConfigOptions()
+opt.set_urdf(urdf)
+opt.set_srdf(srdf)
+opt.generate_jidmap()
+opt.set_bool_parameter('is_model_floating_base', True)
+opt.set_string_parameter('model_type', 'RBDL')
+opt.set_string_parameter('framework', 'ROS')
+model = xbot.ModelInterface(opt)
+robot = xbot.RobotInterface(opt)
+
+robot.sense()
+model.syncFrom(robot)
+model.update()
+
+# CREATE CARTESIAN SOLVER
+freq = 100
+ctrl_points = {0: 'l_sole', 1: 'r_sole'}
+ci_solver = CartesianInterfaceSolver(model=model, robot=robot, ik_dt=1./freq, ctrl_points=ctrl_points)
+print 'Created cartesian interface.'
+
+ctrl_tasks, com_task = ci_solver.getTasks()
+
+l_foot_initial = Affine3()
+l_foot_initial.translation = model.getPose(ctrl_tasks[0].getName()).translation
+l_foot_initial.linear = model.getPose(ctrl_tasks[0].getName()).linear
+
+r_foot_initial = Affine3()
+r_foot_initial.translation = model.getPose(ctrl_tasks[1].getName()).translation
+r_foot_initial.linear = model.getPose(ctrl_tasks[1].getName()).linear
+
+com_initial = model.getCOM()
+
+# ============================================== #
+
+initial_ds = 0.2
+ss_1 = 0.5
+ds_1 = 0.
+ss_2 = 0.
+final_ds = 0.4
+T_total = initial_ds + ss_1 + ds_1 + ss_2 + final_ds
+
+stp = Stepper(initial_ds, ss_1, ds_1, ss_2, final_ds)
+
+initial_l_foot = np.array([l_foot_initial.translation[0], l_foot_initial.translation[1], 0.])
+initial_r_foot = np.array([r_foot_initial.translation[0], r_foot_initial.translation[1], 0.])
+
+print('initial_l_foot:', initial_l_foot)
+print('initial_r_foot:', initial_r_foot)
+
+heigth_com = model.getCOM()[2] - model.getPose(ctrl_tasks[0].getName()).translation[2]
+
+initial_com = np.array([[model.getCOM()[0],  model.getCOM()[1]], [0.2, 0.], [0., 0.]])
+
+print('initial_com:', initial_com)
+
+problem, initial_guess, constraints = stp.generateProblem(initial_com=initial_com, heigth_com=heigth_com, l_foot=initial_l_foot, r_foot=initial_r_foot)
+
+w_opt = stp.solve(problem, initial_guess, constraints)
+
+# stp.plot(w_opt)
+
+com_states, feet_states = stp.get_relevant_variables(w_opt)
+
+## SHOW INITIAL AND FINAL POSITIONS OF FEET
+# for i in range(len(feet_states)):
+#     plt.scatter(feet_states[i]['l'][0], feet_states[i]['l'][1])
+#     plt.scatter(feet_states[i]['r'][0], feet_states[i]['r'][1])
+# plt.show()
+# exit()
+# interpolating stuff
+com_interpol = cs.DM(initial_com.flatten())
+
+multiplier = 2
+dt_int = stp.T / stp.N / 1 / multiplier
+integrator = stp.RK4(1, 1, dt_int)
+#
+for i in range(len(com_states)):
+    for multi_i in range(multiplier):
+        Fk = integrator(x0=com_interpol[:, -1], p=com_states[i]['j'])
+        com_interpol = cs.horzcat(com_interpol, Fk['xf'])
+
+
+com_traj_x = com_interpol[0, :].full().flatten()
+com_traj_y = com_interpol[1, :].full().flatten()
+
+
+t, l_foot_traj = stp.interpolator(step_i=feet_states[0]['l'], step_f=feet_states[1]['l'], step_height=0.05, time=T_total, t_i =initial_ds, t_f=initial_ds+ss_1, freq=freq)
+
+
+lfoot = Affine3()
+lfoot.translation = l_foot_initial.translation
+lfoot.linear = l_foot_initial.linear
+
+com = Affine3()
+com.translation = com_initial
+
+print('number of commands for a T =', T_total, 'trajectory with f =', freq, ': ', int(T_total*freq))
+if len(l_foot_traj['x']) == len(l_foot_traj['y']) == len(l_foot_traj['z']):
+    print('duration of foot trajectory is:', ss_1, '. Trajectory vector of length:', len(l_foot_traj['x']))
+
+if len(com_traj_x) == len(com_traj_y):
+    print('duration of com trajectory is', T_total, '. Trajectory vector of length:', len(com_traj_x))
+
+intial_t = 0
+t = 0
+i = 0
+rate = rospy.Rate(freq)
+#lambda
+
+
+i = 0
+while t < (intial_t + T_total):
+
+    # set l_foot trajectory
+    lfoot.translation[0] = l_foot_traj['x'][i]
+    lfoot.translation[1] = l_foot_traj['y'][i]
+    lfoot.translation[2] = l_foot_initial.translation[2] + l_foot_traj['z'][i]
+
+    # set com trajectory
+    com.translation[0] = com_traj_x[i]
+    com.translation[1] = com_traj_y[i]
+
+    ci_solver.sendTrajectory(com_task, com)
+    ci_solver.sendTrajectory(ctrl_tasks[0], lfoot)
+
+
+    # print('LEFT FOOT:')
+    # print('commanded:', lfoot.translation[0])
+    # print('sensed:', model.getPose(ctrl_tasks[0].getName()).translation)
+    #
+    # print('COM:')
+    # print('commanded:', com.translation[0])
+    # print('sensed:', model.getCOM())
+
+    # print('\n')
+
+
+
+    t += 1./freq
+    i += 1
+
+    rate.sleep()
+
