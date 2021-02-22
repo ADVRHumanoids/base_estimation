22071b97d64a0be31c8fb3b7a9de1ebcd2f3761b
EnricoMingo <enricomingo@gmail.com>
2021-02-22 12:57:39 +0100

    added subscriber to contact status

--- python/static_wpg.py
+++ python/static_wpg.py
@@ -4,6 +4,20 @@ import cartesian_interface as ci
 import rospy
 from cartesian_interface.pyci_all import *
 import time
+from base_estimation.msg import ContactsStatus
+import threading
+
+def thread_func():
+    rospy.spin()
+
+def contacts_status_cb(data):
+    global phase
+
+    contacts = data.contacts_status
+    for contact in contacts:
+        print contact.header.frame_id, ": ", contact.status
+        if phase == 0:
+            #CHANGE CONTACTS STATUS TO ID ONLY IN PHASE0
 
 rospy.init_node('static_wpg', anonymous=True)
 
@@ -22,6 +36,12 @@ print "com: ", com_position.translation
 print "l_foot: ", l_sole_pose.translation
 print "r_foot: ", r_sole_pose.translation
 
+phase = 0
+rospy.Subscriber("/odometry/contacts/status", ContactsStatus, contacts_status_cb)
+x = threading.Thread(target=thread_func)
+x.start()
+
+
 waypoints = []
 waypoints.append(pyci.WayPoint(Affine3(pos=[com_position.translation[0], l_sole_pose.translation[1], com_position.translation[2]]), 10.))
 
@@ -29,12 +49,14 @@ com.setWayPoints(waypoints)
 
 raw_input("Press for rise leg")
 
+phase = 1
 waypoints = []
 waypoints.append(pyci.WayPoint(Affine3(pos=[r_sole_pose.translation[0], r_sole_pose.translation[1], r_sole_pose.translation[2]+0.1]), 10.))
 
 r_sole.setWayPoints(waypoints)
 
 raw_input("Press for lower leg")
+phase = 2
 waypoints = []
 waypoints.append(pyci.WayPoint(Affine3(pos=[r_sole_pose.translation[0]+0.05, r_sole_pose.translation[1]-0.05, r_sole_pose.translation[2]]), 10.))
 r_sole.setWayPoints(waypoints)
