68537f9a7cb22b284e25e181de62590a71decbb1
francesco.ruscelli <francesco.ruscelli@iit.it>
2021-03-02 15:20:30 +0100

    velocity was not scaled with duration. Fixed

--- python/horizon.py
+++ python/horizon.py
@@ -45,33 +45,32 @@ def interpolator(step_i, step_f, step_height, time, t_i, t_f, freq):
 
     t = np.linspace(0, 1, math.ceil(traj_len))
 
-    traj['x'] = np.append(traj['x'], (step_i[0] + (((6 * t - 15) * t + 10) * t ** 3) * (step_f[0] - step_i[0])))  # on the x
-    traj['y'] = np.append(traj['y'], (step_i[1] + (((6 * t - 15) * t + 10) * t ** 3) * (step_f[1] - step_i[1]))) # on the y
-    traj['z'] = np.append(traj['z'], (64 * t ** 3. * (1 - t) ** 3) * step_height) # on the z
 
+    traj['x'] = np.append(traj['x'], (step_i[0] + (((6. * t - 15.) * t + 10.) * t ** 3.) * (step_f[0] - step_i[0])))  # on the x
+    traj['y'] = np.append(traj['y'], (step_i[1] + (((6. * t - 15.) * t + 10.) * t ** 3.) * (step_f[1] - step_i[1]))) # on the y
+    traj['z'] = np.append(traj['z'], (64. * t ** 3. * (1. - t) ** 3.) * step_height) # on the z
 
     traj['x'] = np.append(traj['x'], np.full(traj_len_after, step_f[0]))
     traj['y'] = np.append(traj['y'], np.full(traj_len_after, step_f[1]))
     traj['z'] = np.append(traj['z'], np.full(traj_len_after, 0.))
 
-
-    # compute velocity
-    traj['dx'] = np.append(traj['dx'], 30 * (t - 1) ** 2 * t ** 2 * (step_f[0] - step_i[0]))
-    traj['dy'] = np.append(traj['dy'], 30 * (t - 1) ** 2 * t ** 2 * (step_f[1] - step_i[1]))
-    traj['dz'] = np.append(traj['dz'], step_height * (t - 1) ** 2 * (t ** 2.0 * (192.0 - 192.0 * t) - 192 * t ** 3.0))
+    # compute velocity # todo remember this, derivative of function and then (t_f - t_i) to make it consistent with the parametrization
+    traj['dx'] = np.append(traj['dx'], 30. * (t - 1.) ** 2. * t ** 2. * (step_f[0] - step_i[0])) / (t_f - t_i)
+    traj['dy'] = np.append(traj['dy'], 30. * (t - 1.) ** 2. * t ** 2. * (step_f[1] - step_i[1])) / (t_f - t_i)
+    traj['dz'] = np.append(traj['dz'], step_height * (t - 1.) ** 2. * (t ** 2.0 * (192. - 192. * t) - 192. * t ** 3.)) / (t_f - t_i)
 
     traj['dx'] = np.append(traj['dx'], np.full(traj_len_after, 0.))
     traj['dy'] = np.append(traj['dy'], np.full(traj_len_after, 0.))
     traj['dz'] = np.append(traj['dz'], np.full(traj_len_after, 0.))
 
-    # computeration
-    traj['ddx'] = np.append(traj['ddx'], 60 * t * (2 * t ** 2 - 3 * t + 1) * (step_f[0] - step_i[0]))
-    traj['ddy'] = np.append(traj['ddy'], 60 * t * (2 * t ** 2 - 3 * t + 1) * (step_f[1] - step_i[1]))
-    traj['ddz'] = np.append(traj['ddz'], step_height * (384.0 * t ** 1.0 - 2304.0 * t ** 2.0 + 3840.0 * t ** 3.0 - 1920.0 * t ** 4.0))
-
-    traj['ddx'] = np.append(traj['ddx'], np.full(traj_len_after, 0.))
-    traj['ddy'] = np.append(traj['ddy'], np.full(traj_len_after, 0.))
-    traj['ddz'] = np.append(traj['ddz'], np.full(traj_len_after, 0.))
+    # compute acceleration
+    # traj['ddx'] = np.append(traj['ddx'], 60 * t * (2 * t ** 2 - 3. * t + 1) * (step_f[0] - step_i[0]))
+    # traj['ddy'] = np.append(traj['ddy'], 60 * t * (2 * t ** 2 - 3. * t + 1) * (step_f[1] - step_i[1]))
+    # traj['ddz'] = np.append(traj['ddz'], step_height * (384.0 * t ** 1.0 - 2304.0 * t ** 2.0 + 3840.0 * t ** 3.0 - 1920.0 * t ** 4.0))
+    #
+    # traj['ddx'] = np.append(traj['ddx'], np.full(traj_len_after, 0.))
+    # traj['ddy'] = np.append(traj['ddy'], np.full(traj_len_after, 0.))
+    # traj['ddz'] = np.append(traj['ddz'], np.full(traj_len_after, 0.))
 
     # small hack for t filling
     t = np.linspace(0, 1, len(traj['x']))
@@ -252,15 +251,15 @@ class Problem:
 
     def solveProblem(self, w, g):
 
-        print('================')
-        print('w:', w.shape)
-        print('lbw:', len(self.lbw))
-        print('ubw:', len(self.ubw))
-        print('w0:', len(self.w0))
-
-        print('g:', g.shape)
-        print('lbg:', len(self.ct.lbg))
-        print('ubg:', len(self.ct.ubg))
+        # print('================')
+        # print('w:', w.shape)
+        # print('lbw:', len(self.lbw))
+        # print('ubw:', len(self.ubw))
+        # print('w0:', len(self.w0))
+        #
+        # print('g:', g.shape)
+        # print('lbg:', len(self.ct.lbg))
+        # print('ubg:', len(self.ct.ubg))
         #
         # print('================')
         # print('w:', w)
@@ -422,7 +421,10 @@ class Problem:
         num_var = sum([var[0] for name, var in prb_vars_ordered.items()])
         opt_values = dict()
         for name, var in prb_vars_ordered.items():
-            opt_values[name] = np.zeros([var[0], self.N])
+            if name == 'u':
+                opt_values[name] = np.zeros([var[0], self.N-1])
+            else:
+                opt_values[name] = np.zeros([var[0], self.N])
 
         # fill first N-1 values
         for k in range(self.N-1):
