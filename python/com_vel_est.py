ac42ae22785ff20f3fe82d0811ea20711d9165ba
francesco.ruscelli <francesco.ruscelli@iit.it>
2021-03-03 11:16:57 +0100

    added com_vel_est for com velocity estimation using base_estimation

---
+++ python/com_vel_est.py
@@ -0,0 +1,116 @@
+import xbot_interface.config_options as xbot_opt
+import rospy
+from cartesian_interface.pyci_all import *
+from xbot_interface import xbot_interface as xbot
+from xbot_interface import config_options as co
+from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
+from ci_solver import CartesianInterfaceSolver
+import numpy as np
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TwistStamped
+
+import matplotlib.pyplot as plt
+
+class FloatingBase:
+
+    def __init__(self):
+
+        # rospy.wait_for_message("/odometry/base_link/pose", PoseStamped)
+        # rospy.wait_for_message("/odometry/base_link/twist", TwistStamped)
+
+        self.fb_pose = Affine3()
+        self.fb_twist = np.zeros([6,1])
+
+        rospy.Subscriber("/odometry/base_link/pose", PoseStamped, self._fbPoseCallback)
+        rospy.Subscriber("/odometry/base_link/twist", TwistStamped, self._fbTwistCallback)
+
+        rospy.wait_for_message("/odometry/base_link/pose", PoseStamped)
+        rospy.wait_for_message("/odometry/base_link/twist", TwistStamped)
+        #
+        # print('porcpdioo', self.fb_pose)
+
+
+    def _fbPoseCallback(self, data):
+
+        self.fb_pose.translation[0] = data.pose.position.x
+        self.fb_pose.translation[1] = data.pose.position.y
+        self.fb_pose.translation[2] = data.pose.position.z
+
+        self.fb_pose.quaternion[0] = data.pose.orientation.x
+        self.fb_pose.quaternion[1] = data.pose.orientation.y
+        self.fb_pose.quaternion[2] = data.pose.orientation.z
+        self.fb_pose.quaternion[3] = data.pose.orientation.w
+
+
+    def _fbTwistCallback(self, data):
+        self.fb_twist[0] = data.twist.linear.x
+        self.fb_twist[1] = data.twist.linear.y
+        self.fb_twist[2] = data.twist.linear.z
+        self.fb_twist[3] = data.twist.angular.x
+        self.fb_twist[4] = data.twist.angular.y
+        self.fb_twist[5] = data.twist.angular.z
+
+
+
+    def getFbPose(self):
+
+        return self.fb_pose
+
+    def getFbTwist(self):
+        return self.fb_twist
+
+if __name__ == '__main__':
+
+    rospy.init_node('fb_est')
+    roscpp_init('fb_est', [])
+
+    rate = rospy.Rate(100)
+
+    # PREPARE ROBOT
+    opt = xbot_opt.ConfigOptions()
+
+    urdf = rospy.get_param('/xbotcore/robot_description')
+    srdf = rospy.get_param('/xbotcore/robot_description_semantic')
+
+    opt = co.ConfigOptions()
+    opt.set_urdf(urdf)
+    opt.set_srdf(srdf)
+    opt.generate_jidmap()
+    opt.set_bool_parameter('is_model_floating_base', True)
+    opt.set_string_parameter('model_type', 'RBDL')
+    opt.set_string_parameter('framework', 'ROS')
+    model = xbot.ModelInterface(opt)
+    robot = xbot.RobotInterface(opt)
+
+
+    fb = FloatingBase()
+
+    robot.sense()
+    model.syncFrom(robot)
+    model.setFloatingBaseState(fb.getFbPose(), fb.getFbTwist())
+    model.update()
+
+
+    # t_start = rospy.Time.now().to_sec()
+    # t_plot = [t_start]
+    # com_plot = [model.getCOM()[0]]
+    threshold = 0.2
+    print('starting to loop ...')
+    t = 0
+    while t < 5.:
+
+        model.syncFrom(robot)
+        model.setFloatingBaseState(fb.getFbPose(), fb.getFbTwist())
+        model.update()
+
+        # print(model.getCOM)
+        if model.getCOMVelocity()[0] >= threshold:
+            print('PORCODDIOTROPPOVELOCE')
+        # t = rospy.Time.now().to_sec() - t_start
+
+        # t_plot.append(t)
+        # com_plot.append(model.getCOM()[0])
+
+
+        rate.sleep()
+
