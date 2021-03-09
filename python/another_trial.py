2801d3fefed61856f4220783890c0b48660d88a5
francesco.ruscelli <francesco.ruscelli@iit.it>
2021-03-09 10:37:01 +0100

    nothing important, just tries

--- python/another_trial.py
+++ python/another_trial.py
@@ -1,8 +1,10 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 import casadi as cs
 import numpy as np
 
 import time
+import sympy as sp
+import matplotlib.pyplot as plt
 
 
 def RK4(M, L, x, u, xdot, dt):
@@ -34,7 +36,6 @@ def RK4(M, L, x, u, xdot, dt):
     return cs.Function('F', [X0, U], [X, Q], ['x0', 'p'], ['xf', 'qf'])
 
 
-
 class Problem:
 
     def __init__(self, N):
@@ -51,7 +52,6 @@ class Problem:
 
         self.var_opt = dict()
         self.fun_opt = dict()
-
 
         self.var_dim = dict()
 
@@ -83,12 +83,12 @@ class Problem:
 
     def buildProblem(self):
 
-        w = cs.vertcat(*self.var_opt['x'], *self.var_opt['u'])
+        # w = cs.vertcat(*self.var_opt['x'], *self.var_opt['u'])
 
         self.lbw += [-cs.inf] * self.var_dim['x'] * self.N + [-cs.inf] * self.var_dim['u'] * self.N
         self.ubw += [cs.inf] * self.var_dim['x'] * self.N + [cs.inf] * self.var_dim['u'] * self.N
 
-        return w
+        # return w
 
     def solveProblem(self, w, g):
 
@@ -113,10 +113,10 @@ class Problem:
 
         # print(w_opt)
 
+
 class Constraint:
 
     def __init__(self):
-
         self.g = list()
         self.lbg = list()
         self.ubg = list()
@@ -124,7 +124,6 @@ class Constraint:
         self.g_dict = dict()
 
     def setConstraintFunction(self, name, g):
-
         flat_list = [item for sublist in list(self.var_opt.values()) for item in sublist]
         f = cs.Function(name, flat_list, [g])
         self.g_dict[name] = f
@@ -132,7 +131,6 @@ class Constraint:
         return f
 
     def addConstraint(self, name):
-
         f = self.g_dict[name]
         g = f(*list(self.var_opt.values()))
         print(g)
@@ -141,64 +139,74 @@ class Constraint:
         return g
 
     def setConstraintBounds(self, lbg, ubg, nodes):
-
         self.lbg[nodes[0]:nodes[1]] = [lbg] * (nodes[1] - nodes[0])
         self.ubg[nodes[0]:nodes[1]] = [ubg] * (nodes[1] - nodes[0])
 
     def setConstraint(self, g):
-
         self.g.append(g)
 
     def getConstraints(self):
-
         return cs.vertcat(*self.g)
 
     def update(self, var):
-
         self.var_opt = var
 
 
-
 if __name__ == '__main__':
+    # N = 2
+    # prb = Problem(N)
+    # h = 1
+    # grav = 9.8
+    a = sp.symbols('a')
+    b = sp.symbols('b')
+    t = sp.symbols('t')
+    b = 0.306179839802
+    a = -0.0936315503011
 
-    N = 2
-    prb = Problem(N)
-    h = 1
-    grav = 9.8
+    # b = 2
+    # a = 0
+    fun = a + ((6 * t - 15) * t + 10) * t ** 3 * (b-a)
+    dfun = sp.diff(fun, t)
 
-    # p = cs.SX.sym('p', 2)  # com position
-    # v = cs.SX.sym('v', 2)  # com velocity
-    # a = cs.SX.sym('a', 2)  # com acceleration
-    # define state variables
-    # x = cs.vertcat(p, v, a)
+    f1 = sp.plot(fun, (t, 0,1), show=False)
+    f2 = sp.plot(dfun, (t, 0,1), show=False)
 
-    # define control variables
-    # j = cs.SX.sym('j', 2)  # com jerk
-    # u = j  # control
-    # model equation
-    # xdot = cs.vertcat(v, a, j)
-
-    # integrator = RK4(1, 1, x, u, xdot, 0.01)
-
-    prb.setVariable('x', 6)
-    prb.setVariable('u', 2)
-
-
-    var_opt = prb.getVariable()
-
-    for k in range(N):
-        if k > 0:
-            zmp_old = var_opt['x'][k-1][0:2] - var_opt['x'][k-1][4:6] #* (h / grav)
-            # prb.ct.setConstraint(zmp_old - var_opt['u'][k])
-
-        zmp = var_opt['x'][k][0:2] - var_opt['x'][k][4:6] #* (h / grav)
-
-        # prb.ct.setConstraint(var_opt['x'][k][0:2] - var_opt['x'][k][4:6])
-        # prb.ct.setConstraint(var_opt['u'][k] - var_opt['x'][k][4:6])
-
-
-    w = prb.buildProblem()
-    g = prb.ct.getConstraints()
-
-
-    w_opt = prb.solveProblem(w, g)
+    print(dfun)
+    f1.append(f2[0])
+    f1.show()
+    # # p = cs.SX.sym('p', 2)  # com position
+    # # v = cs.SX.sym('v', 2)  # com velocity
+    # # a = cs.SX.sym('a', 2)  # com acceleration
+    # # define state variables
+    # # x = cs.vertcat(p, v, a)
+    #
+    # # define control variables
+    # # j = cs.SX.sym('j', 2)  # com jerk
+    # # u = j  # control
+    # # model equation
+    # # xdot = cs.vertcat(v, a, j)
+    #
+    # # integrator = RK4(1, 1, x, u, xdot, 0.01)
+    #
+    # prb.setVariable('x', 6)
+    # prb.setVariable('u', 2)
+    #
+    #
+    # var_opt = prb.getVariable()
+    #
+    # for k in range(N):
+    #     if k > 0:
+    #         zmp_old = var_opt['x'][k-1][0:2] - var_opt['x'][k-1][4:6] #* (h / grav)
+    #         # prb.ct.setConstraint(zmp_old - var_opt['u'][k])
+    #
+    #     zmp = var_opt['x'][k][0:2] - var_opt['x'][k][4:6] #* (h / grav)
+    #
+    #     # prb.ct.setConstraint(var_opt['x'][k][0:2] - var_opt['x'][k][4:6])
+    #     # prb.ct.setConstraint(var_opt['u'][k] - var_opt['x'][k][4:6])
+    #
+    #
+    # w = prb.buildProblem()
+    # g = prb.ct.getConstraints()
+    #
+    #
+    # w_opt = prb.solveProblem(w, g)
